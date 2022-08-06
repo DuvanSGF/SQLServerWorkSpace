d3588e9ed40c40f29af52bdcef62a0364bceda3e
DuvanSGF <Damejia.98@hotmail.com>
2022-08-05 20:59:15 -0500

    Ejercicio de prueba

--- 2 - Legenda/WithNolock.sql
+++ 2 - Legenda/WithNolock.sql
@@ -1,5 +1,5 @@
 --Es importante conocer las buenas practicas de conocer
--- de la funcion with Nolock
+-- de la funcion with Nolock.
 -- https://www.sqlshack.com/es/sugerencias-de-tabla-de-sql-server-con-nolock-las-mejores-practicas/
 
 SELECT * FROM LockTestDemo WITH (NOLOCK)
