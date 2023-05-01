cc78af2b3fe17052f9c7ba02e3c10eab98a8bc02
DuvanSGF <Damejia.98@hotmail.com>
2023-04-30 22:28:19 -0500

    :ambulance: Day 3 leetcode - String Processing Functions-

--- 2 - Legenda/LeetCode/SQLStudy/SQL_I/Day2_Select&Order_Exercises.sql
+++ 2 - Legenda/LeetCode/SQLStudy/SQL_I/Day2_Select&Order_Exercises.sql
@@ -4,3 +4,25 @@ Select employee_id,
 bonus = (CASE WHEN employee_id % 2 = 0 or name LIKE 'm%' THEN 0 ELSE salary END)
 from Employees
 order by employee_id
+
+--627. Swap Salary
+--https://leetcode.com/problems/swap-salary/description/?envType=study-plan&id=sql-i
+Update salary 
+set sex = (Case WHEN sex = 'f' then 'm' else 'f' end)
+
+--196. Delete Duplicate Emails
+--https://leetcode.com/problems/delete-duplicate-emails/?envType=study-plan&id=sql-i
+with cteTemp as(
+Select id, email,
+rank() over(partition By email order By id ASC) as THERANK
+from Person
+)
+
+Delete from Person where id in (Select id from cteTemp where THERANK <> 1)
+
+--1667. Fix Names in a Table
+--https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan&id=sql-i
+Select user_id,
+name = SUBSTRING(Upper(name),1,1) + LOWER(SUBSTRING(name, 2, LEN(name))) 
+from Users
+order by user_id asc
