0080611df851f4d6c032670aa7c01011ba49de5c
DuvanSGF <Damejia.98@hotmail.com>
2023-05-23 20:54:06 -0500

    :fire: Study SP_EXECUTESQL

---
+++ 2 - Legenda/LeetCode/Exercise#577.sql
@@ -0,0 +1,23 @@
+-- =============================================
+-- Author:		In. Duván Albeiro Mejia Cortes
+-- Create date: 22-05-2023
+-- Description:	5577. Employee Bonus
+-- ============================================= 
+
+--https://leetcode.com/problems/employee-bonus/description/
+
+/* Write your T-SQL query statement below */
+--Solution 1 - Oprtimized
+Select A.name, B.bonus from Employee A
+LEFT JOIN bonos B on A.empId = B.empID
+where B.bonus < 1000 or bonus is null 
+
+
+--Solution 2
+Select A.name, B.bonus from Employee A
+LEFT JOIN Bonus B on A.empId = B.empID
+Where A.empId Not IN (
+Select B.empID from Employee A
+LEFT JOIN Bonus B on A.empId = B.empID
+Where B.bonus >= 1000 
+)
---
+++ 2 - Legenda/SP/sp_ExecuteSQL.sql
@@ -0,0 +1,35 @@
+-- =============================================
+-- Author:		In. Duván Albeiro Mejia Cortes
+-- Create date: 23-05-2023
+-- Description:	SP_EXECUTESQL
+-- ============================================= 
+
+--Definition
+/*
+[ @stmt= ] statement
+Is a Unicode string that contains a Transact-SQL statement or batch. @stmt must be either a Unicode constant or a Unicode variable. 
+More complex Unicode expressions, such as concatenating two strings with the + operator, are not allowed. 
+Character constants are not allowed. If a Unicode constant is specified, it must be prefixed with an N. 
+For example, the Unicode constant N'sp_who' is valid, but the character constant 'sp_who' is not. 
+The size of the string is limited only by available database server memory. On 64-bit servers, 
+the size of the string is limited to 2 GB, the maximum size of nvarchar(max).
+*/
+
+--Exercise #1: Creating a Basic SP and understanding the concepts
+Alter PROCEDURE  SPGetHeoresByID
+@ids varchar(100)
+AS
+declare @String NVarchar(100) = Null, @Parameters Nvarchar(100) = Null
+SET NOCOUNT ON;
+Begin
+
+SET @String = N'Select * from Heros Where id = @HeroesIDs'
+Set @Parameters = N'@HeroesIDs varchar(100)'
+
+Exec sp_executesql @String,@Parameters, @HeroesIDs = @ids
+End
+
+EXEC SPGetHeoresByID '1'
+
+--Exercise #2: Creating a SP with more ids entry and return a data
+--Exercise #3: SP with Try Catch and most advanced Structure
