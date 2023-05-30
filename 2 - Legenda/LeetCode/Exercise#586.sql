4be45a374a0a683f0d59d7971f6af01c75058f9d
DuvanSGF <Damejia.98@hotmail.com>
2023-05-30 16:58:14 -0500

    :zap: LeetCode Exercise

---
+++ 2 - Legenda/LeetCode/Exercise#586.sql
@@ -0,0 +1,13 @@
+-- =============================================
+-- Author:		In. Duván Albeiro Mejia Cortes
+-- Create date: 30-05-2023
+-- Description:	586. Customer Placing the Largest Number of Orders
+-- ============================================= 
+
+--https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/description/
+Select Top 1 A.Customer_number from 
+(
+Select Customer_number,Count(Customer_number) as TotalPedidos from Orders
+Group By customer_number
+) as A
+Order By A.TotalPedidos DESC
