c6fa9720cc07138971fa3b2545641fc00370d43b
DuvanSGF <Damejia.98@hotmail.com>
2023-06-12 17:55:15 -0500

    :zap: LeetCode Exercise - Medium

---
+++ 2 - Legenda/LeetCode/Exercise#602.Friend Requests II_ Who Has the Most Friends_Medium.sql
@@ -0,0 +1,18 @@
+-- =============================================
+-- Author:		In. Duván Albeiro Mejia Cortes
+-- Create date: 12-06-2023
+-- Description:	602. Friend Requests II: Who Has the Most Friends
+-- Nivel: Medium
+-- ============================================= 
+
+--https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/description/
+
+Select Top 1 id,Sum(num) as num from (
+Select requester_id as id,Count(requester_id) AS num from RequestAccepted
+Group By requester_id
+UNION ALL
+Select accepter_id as id,Count(accepter_id) AS num from RequestAccepted
+Group By accepter_id) T
+Group By id
+Order BY 2 DESC
+
